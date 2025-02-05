6d8d079edee1253eb06ce8bdbf4f7267daf6581e
extreemo100 <58828956+extreemo100@users.noreply.github.com>
2025-02-05 06:46:05 +0500

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: 'pikachu.mind',
+      imageTargetSrc: 'can.mind',
     });
     const {renderer, scene, camera} = mindarThree;
 
